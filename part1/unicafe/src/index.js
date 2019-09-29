5a6453a76fcecb80c4ad11efcd4b086059b96df2
k-kamada <kamadak0404@gmail.com>
2019-09-29 12:12:02 +0900

    Done exercise 1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Statistics = ({ text, value }) => {
+  const terminalChar = (text === 'positive') ? '%' : '';
+  return (
+    <>
+      {text} {value} {terminalChar} <br />
+    </>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -17,12 +26,12 @@ const App = () => {
       <button onClick={()=>{setBad(bad+1); setAll(all+1)}}>bad</button>
       <h1>statistics</h1>
       <p>
-        good {good}<br />
-        neutral {neutral}<br />
-        bad {bad}<br />
-        all {all}<br />
-        average {average}<br />
-        posirive {good / all * 100} %
+        <Statistics text='good' value={good} />
+        <Statistics text='neutral' value={neutral} />
+        <Statistics text='bad' value={bad} />
+        <Statistics text='all' value={all} />
+        <Statistics text='average' value={average} />
+        <Statistics text='positive' value={good / all * 100} />
       </p>
     </div>
   );
