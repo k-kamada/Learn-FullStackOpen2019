a11a010043869ce0ed2a78239f45711daf6cb2d5
k-kamada <kamadak0404@gmail.com>
2019-09-29 12:03:31 +0900

    Done exercise 1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,18 +5,24 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const average = (good - bad) / all;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={()=>setGood(good+1)}>good</button>
-      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <button onClick={()=>{setGood(good+1); setAll(all+1)}}>good</button>
+      <button onClick={()=>{setNeutral(neutral+1); setAll(all+1)}}>neutral</button>
+      <button onClick={()=>{setBad(bad+1); setAll(all+1)}}>bad</button>
       <h1>statistics</h1>
       <p>
         good {good}<br />
         neutral {neutral}<br />
-        bad {bad}
+        bad {bad}<br />
+        all {all}<br />
+        average {average}<br />
+        posirive {good / all * 100} %
       </p>
     </div>
   );
