c35d6a696be8d474e93396bd73adb891d342e06b
k-kamada <kamadak0404@gmail.com>
2019-12-08 23:57:56 +0900

    Support .env

--- part3/text/index.js
+++ part3/text/index.js
@@ -1,49 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const Note = require('./models/note');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('build'));
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
 
 app.get('/api', (req, res) => {
   res.send('<h1>Hello world!</h1>');
 });
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  Note.find({}).then(notes => {
+    res.json(notes.map(note => note.toJSON()));
+  });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON());
+  });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -70,19 +49,18 @@ app.post('/api/notes', (request, response) => {
     });
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note ({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-  }
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON());
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server runnning on port ${PORT}`)
 });
