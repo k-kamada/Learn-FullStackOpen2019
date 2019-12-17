bb4cdb851c1e419d528f1dfd59e45f1498987603
k-kamada <kamadak0404@gmail.com>
2019-12-17 14:14:26 +0900

    Done exercise 4.2: Blog list, step2

---
+++ part4-bloglist/app.js
@@ -0,0 +1,17 @@
+const config = require('./utils/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const mongoose = require('mongoose');
+
+console.log(`connecting to ${config.MONGODB_URI}`);
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
