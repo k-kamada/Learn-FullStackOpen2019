53cc388ac389ab0379c6b321373829c3696b1852
k-kamada <kamadak0404@gmail.com>
2019-12-18 13:16:44 +0900

    Done exercise 4.4: helper functions and unit tests, step2

--- part4-bloglist/utils/list_helper.js
+++ part4-bloglist/utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, current) => {
+    return sum + current.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
