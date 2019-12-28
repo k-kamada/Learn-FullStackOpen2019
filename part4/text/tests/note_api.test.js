50cf03158ff4587cd919ba38709f9fd682fe26ba
k-kamada <kamadak0404@gmail.com>
2019-12-29 01:57:33 +0900

    part4b: Optimizing the beforeEach function

--- part4/text/tests/note_api.test.js
+++ part4/text/tests/note_api.test.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const helper = require('./test_helper');
 const app = require('../app');
-
 const api = supertest(app);
+
+const Note = require('../models/note');
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note));
+  const promiseArray = noteObjects.map(note => note.save());
+  await Promise.all(promiseArray);
+});
 
 test('notes are returned as json', async () => {
   await api
@@ -11,16 +22,86 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 });
 
-test('there are four notes', async () => {
+test('all notes are returned', async () => {
   const response = await api.get('/api/notes');
 
-  expect(response.body.length).toBe(4);
+  expect(response.body.length).toBe(helper.initialNotes.length);
 });
 
-test('the first note is about HTTP methods', async () => {
+test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes');
+  const contents = response.body.map(r => r.content);
 
-  expect(response.body[0].content).toBe('HTML is easy');
+  expect(contents).toContain(
+    'Browser can execute only Javascript'
+  );
+});
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true
+  };
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const notesAtEnd = await helper.notesInDb();
+  expect(notesAtEnd.length).toBe(helper.initialNotes.length + 1);
+
+  const contents = notesAtEnd.map(n => n.content);
+  expect(contents).toContain(
+    'async/await simplifies making async calls'
+  );
+});
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  };
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400);
+
+  const notesAtEnd = await helper.notesInDb();
+  expect(notesAtEnd.length).toBe(helper.initialNotes.length);
+});
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb();
+
+  const noteToView = notesAtStart[0];
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  expect(resultNote.body).toEqual(noteToView);
+});
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToDelete = notesAtStart[0];
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204);
+
+  const notesAtEnd = await helper.notesInDb();
+
+  expect(notesAtEnd.length).toBe(
+    helper.initialNotes.length - 1
+  );
+
+  const contents = notesAtEnd.map(r => r.content);
+
+  expect(contents).not.toContain(noteToDelete.content);
 });
 
 afterAll(() => {
